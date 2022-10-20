64b8356a740de163d253fc157902d9f26c8d9e52
Mason Ginter <mason@dagint.com>
2022-10-20 18:58:30 -0400

    First 2 tests for derive_builder

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,27 @@
 use proc_macro::TokenStream;
+use proc_macro2::TokenStream as TokenStream2;
+use syn::{DeriveInput, parse_macro_input};
+use quote::quote;
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+    let ast = parse_macro_input!(input as DeriveInput);
+    let stream = expand(ast)
+        .map_err(|err| err.to_compile_error())
+        .unwrap();
+    stream.into()
+}
 
-    unimplemented!()
+
+fn expand(ast: DeriveInput) -> Result<TokenStream2, syn::Error> {
+    // println!("{:#?}", ast);
+    let struct_ident = ast.ident;
+    let expanded = quote! {
+        impl #struct_ident {
+            fn builder() -> () {
+                ()
+            }
+        }
+    };
+    Ok(expanded)
 }
