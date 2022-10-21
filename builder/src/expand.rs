20051ced55e8dce75c96a4c1df400834d9abef88
Mason Ginter <mason@dagint.com>
2022-10-21 01:39:56 -0400

    Seperate code into `expand.rs` and `structs.rs`

---
+++ builder/src/expand.rs
@@ -0,0 +1,91 @@
+use proc_macro2::TokenStream as TokenStream2;
+use quote::{quote, format_ident};
+use syn::DeriveInput;
+
+use crate::structs::Field;
+
+
+
+
+pub(crate) fn expand(ast: DeriveInput) -> Result<TokenStream2, syn::Error> {
+    // println!("{:#?}", ast);
+    let struct_ident = &ast.ident;
+    let fields = Field::get_multiple_from_ast(&ast)?;
+    // Get identifier for our builder struct. Need for builer impl and original struct's builder method
+    let builder_ident = format_ident!("{struct_ident}Builder");
+    let builder_struct = expand_builder(struct_ident, &builder_ident, &fields);
+    let expanded = quote! {
+        impl #struct_ident {
+            fn builder() -> #builder_ident {
+                #builder_ident::new()
+            }
+        }
+        #builder_struct
+    };
+    Ok(expanded)
+}
+
+fn expand_builder(original_ident: &syn::Ident, builder_ident: &syn::Ident, fields: &Vec<Field>) -> TokenStream2 {
+    let struct_def = expand_builder_struct(builder_ident, fields);
+    let impl_block = expand_builder_impl(original_ident, builder_ident, fields);
+    
+    quote! {
+        #struct_def
+
+        #impl_block
+    }
+}
+
+fn expand_builder_impl(original_ident: &syn::Ident, builder_ident: &syn::Ident, fields: &Vec<Field>) -> TokenStream2 {
+    let fields_init: Vec<TokenStream2> = fields
+        .iter()
+        // TODO: Do error propogating instead of flat_map (which throws away any errors)
+        .flat_map(Field::as_optional_init)
+        .collect();
+    let field_setters: Vec<TokenStream2> = fields
+        .iter()
+        .flat_map(Field::as_setter)
+        .collect();
+    let build_fn = expand_build_method(original_ident, builder_ident, fields);
+    quote! {
+        impl #builder_ident {
+            fn new() -> #builder_ident {
+                #builder_ident {
+                    #(#fields_init),*
+                }
+            }
+            
+            #build_fn
+
+            #(#field_setters)*
+        }
+    }
+}
+
+
+fn expand_build_method(original_ident: &syn::Ident, _builder_ident: &syn::Ident, fields: &[Field]) -> TokenStream2 {
+    let field_inits: Vec<TokenStream2> = fields
+        .iter()
+        .flat_map(Field::as_build_init)
+        .collect();
+    quote! {
+        fn build(&mut self) -> Result<#original_ident, Box<dyn std::error::Error>> {
+            Ok(#original_ident {
+                #(#field_inits),*
+            })
+        }
+    }
+}
+
+fn expand_builder_struct(builder_ident: &syn::Ident, fields: &Vec<Field>) -> TokenStream2 {
+    let builder_fields: Vec<TokenStream2> = fields
+        .iter()
+        // TODO: Do error propogating instead of flat_map (which throws away any errors)
+        .flat_map(Field::as_optional_field)
+        .collect();
+    quote! {
+        struct #builder_ident {
+            #(#builder_fields),*
+        }
+    }
+}
---
+++ builder/src/structs.rs
@@ -0,0 +1,73 @@
+use proc_macro2::TokenStream as TokenStream2;
+use syn::{Type, DeriveInput};
+use quote::{quote, quote_spanned};
+
+
+
+pub(crate) struct Field<'a> {
+    field_ident: &'a proc_macro2::Ident,
+    field_type: &'a Type,
+}
+
+impl<'a> Field<'a> {
+    pub(crate) fn get_multiple_from_ast(ast: &'a DeriveInput) -> Result<Vec<Self>, syn::Error> {
+        // println!("{:#?}", ast.data);
+        match ast.data {
+            syn::Data::Struct(ref data) => {
+                match data.fields {
+                    syn::Fields::Named(ref name_fields) => {
+                        let mut fields = Vec::new();
+                        for field in &name_fields.named {
+                            fields.push(Self::get_field_from_syn(field)?);
+                        }
+                        Ok(fields)
+                    },
+                    syn::Fields::Unnamed(_) => panic!("tuple structs not supported"),
+                    syn::Fields::Unit => panic!("unit structs not supported"),
+                }
+            },
+            // TODO: Add proper compiler error message on struct keyword
+            syn::Data::Enum(_) => panic!("enum is not supported"),
+            syn::Data::Union(_) => panic!("enum is not supported"),
+        }
+    }
+    pub(crate) fn get_field_from_syn(field: &'a syn::Field) -> Result<Self, syn::Error>{
+        Ok(Self {
+            field_ident: field.ident.as_ref().unwrap(),
+            field_type: &field.ty,
+        })
+    }
+
+    pub(crate) fn as_optional_field(&self) -> Result<TokenStream2, syn::Error> {
+        let ident = self.field_ident;
+        let ty = self.field_type;
+        Ok(quote! {
+            #ident: std::option::Option<#ty>
+        })
+    }
+    pub(crate) fn as_optional_init(&self) -> Result<TokenStream2, syn::Error> {
+        let ident = self.field_ident;
+        Ok(quote! {
+            #ident: None
+        })
+    }
+    pub(crate) fn as_build_init(&self) -> Result<TokenStream2, syn::Error> {
+        let ident = self.field_ident;
+        let clone_statement = quote_spanned!(ident.span()=>
+            #ident.clone()
+        );
+        Ok(quote! {
+            #ident: self.#clone_statement.ok_or(String::from(concat!("{} was not set", stringify!(#ident))))?
+        })
+    }
+    pub(crate) fn as_setter(&self) -> Result<TokenStream2, syn::Error> {
+        let ident = self.field_ident;
+        let ty = self.field_type;
+        Ok(quote! {
+            fn #ident(&mut self, val: #ty) -> &mut Self {
+                self.#ident = Some(val);
+                self
+            }
+        })
+    }
+}
